190d765d818c6bb8c854a8d1fbf7d987a2385371
chi chen <schiller_china_chichen@outlook.com>
2026-07-08 11:55:56 +0800

    feat: init project

---
+++ AiChatClient/Services/Impl/ChatService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiChatClient.Services.Impl
+{
+    public class ChatService : IChatService
+    {
+        public Task<string> SendAsync(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
